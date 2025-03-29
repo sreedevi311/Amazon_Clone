9fa939d91730c8e4efea84ade9cc68baea4c802f
sreedevi311 <165983022+sreedevi311@users.noreply.github.com>
2025-03-29 20:37:28 +0530

    Add files via upload

---
+++ deliveryOptions.js
@@ -0,0 +1,13 @@
+export let DeliveryOptions=[{id:'1',
+                         deliveryDays:'7',
+                         deliveryCharge:0},
+                        {
+                        id:'2',
+                        deliveryDays:3,
+                        deliveryCharge:499 
+                        },
+                        {
+                        id:'3',
+                        deliveryDays:1,
+                        deliveryCharge:999
+                        }];
