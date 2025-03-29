dd4e83da10266ada9fe7a81eb8416a8f6b31bdfd
sreedevi311 <165983022+sreedevi311@users.noreply.github.com>
2025-03-29 20:36:29 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,219 @@
+ export const products=[
+{
+    id:'id1',
+    image:'https://supersimple.dev/projects/amazon/images/products/athletic-cotton-socks-6-pairs.jpg',
+    name:'Black and Gray Athletic Cotton Socks- 6 pairs',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png",
+        people:87},
+    priceCents:1046
+},
+{
+    id:'id2',
+    image:'https://supersimple.dev/projects/amazon/images/products/intermediate-composite-basketball.jpg',
+    name:'Intermediate Size Basketball',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-4.png",
+        people:127},
+    priceCents:2095
+},
+{
+    id:'id3',
+    image:'https://supersimple.dev/projects/amazon/images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
+    name:'Adults Plain Cotton T-Shirt - 2 Pack',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png",
+        people:76},
+    priceCents:799
+},
+{
+    id:'id4',
+    image:'https://supersimple.dev/projects/amazon/images/products/black-2-slot-toaster.jpg',
+    name:'2 Slot medium Toaster - Black',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png",
+        people:2197},
+    priceCents:1899
+},
+{
+    id:'id5',
+    image:'https://supersimple.dev/projects/amazon/images/products/6-piece-white-dinner-plate-set.jpg',
+    name:'6 Piece White Dinner Plate Set',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-4.png",
+        people:37},
+    priceCents:2067 
+},
+{
+    id:'id6',
+    image:'https://supersimple.dev/projects/amazon/images/products/6-piece-non-stick-baking-set.webp',
+    name:'6-Piece Nonstick Oven Bakeware Set',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png",
+        people:175},
+    priceCents:3499
+},
+{
+    id:'id7',
+    image:'https://supersimple.dev/projects/amazon/images/products/plain-hooded-fleece-sweatshirt-yellow.jpg',
+    name:'Plain Hooded Fleece Sweatshirt',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png",
+        people:175},
+    priceCents:3499
+},
+{
+    id:'id8',
+    image:'https://supersimple.dev/projects/amazon/images/products/men-athletic-shoes-green.jpg',
+    name:"Men's Slim-Fit Summer Shorts",
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-4.png",
+        people:37},
+    priceCents:2067
+},
+{
+    id:'id9',
+    image:'https://supersimple.dev/projects/amazon/images/products/round-sunglasses-black.jpg',
+    name:'Round Sunglasses',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-5.png",
+        people:2197},
+    priceCents:1899
+},
+{
+    id:'id10',
+    image:'https://supersimple.dev/projects/amazon/images/products/blackout-curtains-black.jpg',
+    name:'Blackout Curtains Set 42 x 84-Inch - Black, 2...',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png",
+        people:56},
+    priceCents:799
+},
+{
+    id:'id11',
+    image:'https://supersimple.dev/projects/amazon/images/products/sky-flower-stud-earrings.webp',
+    name:'Sterling Silver Sky Flower Stud Earrings',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-4.png",
+        people:127},
+    priceCents:2095
+},
+{
+    id:'id12',
+    image:'https://supersimple.dev/projects/amazon/images/products/coffeemaker-with-glass-carafe-black.jpg',
+    name:'Coffeemaker with Glass Carafe and Reusable',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png" ,
+        people:87},
+    priceCents:1046
+},
+{
+    id:'id13',
+    image:'https://supersimple.dev/projects/amazon/images/products/electric-glass-and-steel-hot-water-kettle.webp',
+    name:'Electric Glass and Steel Hot Tea Water Kettle',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-4.png",
+        people:87},
+    priceCents:1046
+},
+{
+    id:'id14',
+    image:'https://supersimple.dev/projects/amazon/images/products/blackout-curtain-set-beige.webp',
+    name:'Blackout Curtains Set 4-Pack - Beige',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-4.png",
+        people:127},
+    priceCents:2095
+},
+{
+    id:'id15',
+    image:'https://supersimple.dev/projects/amazon/images/products/women-beach-sandals.jpg',
+    name:"Women's Two Strap Buckle Sandals - Tan",
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png",
+        people:76},
+    priceCents:799
+},
+{
+    id:'id16',
+    image:'https://supersimple.dev/projects/amazon/images/products/women-stretch-popover-hoodie-black.jpg',
+    name:"Women's Stretch Popover Hoodie",
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-5.png",
+        people:2197},
+    priceCents:1899
+},
+{
+    id:'id17',
+    image:'https://supersimple.dev/projects/amazon/images/products/non-stick-cooking-set-15-pieces.webp',
+    name:'Non-Stick Cookware Set, Pots, Pans and...',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-4.png",
+        people:37},
+    priceCents:2067 
+},
+{
+    id:'id18',
+    image:'https://supersimple.dev/projects/amazon/images/products/duvet-cover-set-blue-twin.jpg',
+    name:'Duvet Cover Set with Zipper Closure',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png",
+        people:175},
+    priceCents:3499
+},
+{
+    id:'id19',
+    image:'https://supersimple.dev/projects/amazon/images/products/women-chunky-beanie-gray.webp',
+    name:"Women's Chunky Cable Beanie - Gray",
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png",
+        people:175},
+    priceCents:3499
+},
+{
+    id:'id20',
+    image:'https://supersimple.dev/projects/amazon/images/products/men-navigator-sunglasses-brown.jpg',
+    name:"Men's Navigator Sunglasses Pilot",
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-4.png",
+        people:37},
+    priceCents:2067
+},
+{
+    id:'id21',
+    image:'https://supersimple.dev/projects/amazon/images/products/round-airtight-food-storage-containers.jpg',
+    name:'Round Airtight Food Storage Containers - 5',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-5.png",
+        people:2197},
+    priceCents:1899
+},
+{
+    id:'id22',
+    image:'https://supersimple.dev/projects/amazon/images/products/double-elongated-twist-french-wire-earrings.webp',
+    name:'Double Oval Twist French Wire Earrings -',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png",
+        people:56},
+    priceCents:799
+},
+{
+    id:'id23',
+    image:'https://supersimple.dev/projects/amazon/images/products/knit-athletic-sneakers-pink.webp',
+    name:'Waterproof Knit Athletic Sneakers - Pink',
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-4.png",
+        people:127},
+    priceCents:2095
+},
+{
+    id:'id24',
+    image:'https://supersimple.dev/projects/amazon/images/products/men-cozy-fleece-zip-up-hoodie-red.jpg',
+    name:"Men's Full-Zip Hooded Fleece Sweatshirt",
+    rating:{
+        stars:"https://supersimple.dev/projects/amazon/images/ratings/rating-45.png" ,
+        people:87},
+    priceCents:1046
+}];
+
+export default products;
